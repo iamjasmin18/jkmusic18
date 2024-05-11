dd658339f40d18ecc5c4eadd27adf153ed85f6ca
GREATPERSON <168116588+greatpersonxd@users.noreply.github.com>
2024-05-11 13:01:26 +0530

    Update imposter.py

--- ANNIEMUSIC/plugins/tools/imposter.py
+++ ANNIEMUSIC/plugins/tools/imposter.py
@@ -86,7 +86,7 @@ async def chk_usr(_, message: Message):
             message.from_user.last_name,
         )
     if msg != "":
-        await message.reply_photo("https://telegra.ph/file/58afe55fee5ae99d6901b.jpg", caption=msg)
+        await message.reply_photo(" https://telegra.ph/file/b7105cbed0c3df14a11ff.jpg", caption=msg)
 
 
 @app.on_message(filters.group & filters.command("imposter") & ~filters.bot & ~filters.via_bot)
